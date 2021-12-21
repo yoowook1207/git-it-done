e5120a368ebf94a293e5d7cfae28351283aa4bf5
Sung Yoo <yoowook1207@gmail.com>
2021-12-21 14:13:18 -0500

    repo sync done

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,13 @@ var repoNameEl = document.querySelector("#repo-name")
 var getRepoName = function() {
     var queryString= document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName
+    if(repoName) {
+        repoNameEl.textContent = repoName
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html")
+    }
 }
 
 var getRepoIssues = function(repo) {
@@ -24,7 +29,8 @@ var getRepoIssues = function(repo) {
                 });
         }
         else {
-            alert("There was a problem with your request!")
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     })
   };
